fd1ffbe62c269c8b2954901ccaa841b618d2386d
Elizabeth <ek@elizabethkillgore.com>
2024-02-02 16:58:44 -0500

    react hooks react router code along v6 updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About"
-import Login from "./pages/Login"
-import UserProfile from "./pages/UserProfile";
-import ErrorPage from "./pages/ErrorPage";
 import routes from "./routes.js"
 
 const router = createBrowserRouter(routes)
